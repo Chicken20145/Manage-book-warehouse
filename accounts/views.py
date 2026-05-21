c368d62d3b3e365f020cd87a71fa04eae8fb2b7c
Chicken20145 <ngoisao3177@gmail.com>
2026-05-21 16:44:44 +0700

    Hoàn thiện app accounts: Tạo CustomUser (Admin, Librarian, Student), giao diện Đăng ký & Đăng nhập

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from.forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Đăng nhập luôn sau khi đăng ký thành công
+            return redirect('login') # Tạm thời chuyển hướng về trang login
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
