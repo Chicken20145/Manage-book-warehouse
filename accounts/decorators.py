c3cec2b5949df1fa8a88467a594d90195909ee04
Chicken20145 <ngoisao3177@gmail.com>
2026-05-21 16:49:29 +0700

    Thêm tính năng Phân quyền (role_required) để bảo vệ các view

---
+++ accounts/decorators.py
@@ -0,0 +1,22 @@
+﻿from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+
+def role_required(*allowed_roles):
+    """
+    Hàm bọc kiểm tra quyền của người dùng.
+    Nhận vào danh sách các role được phép (vd: 'ADMIN', 'LIBRARIAN')
+    """
+    def decorator(view_func):
+        def wrap(request, *args, **kwargs):
+            # 1. Nếu chưa đăng nhập -> Đẩy về trang Login
+            if not request.user.is_authenticated:
+                return redirect('login')
+                
+            # 2. Nếu đã đăng nhập và role nằm trong danh sách cho phép -> Cho đi tiếp
+            if request.user.role in allowed_roles:
+                return view_func(request, *args, **kwargs)
+                
+            # 3. Nếu sai quyền -> Báo lỗi 403 (Hoặc có thể redirect về trang báo lỗi riêng)
+            raise PermissionDenied
+        return wrap
+    return decorator
