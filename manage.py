fa113676ca85548f5d77cdce8c02fc76df67a9c5
Chicken20145 <ngoisao3177@gmail.com>
2026-06-28 10:43:14 +0700

    Run migrations automatically before development server

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
+        from django.core.management import call_command
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
@@ -15,6 +16,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+        import django
+
+        django.setup()
+        print('Checking and applying migrations...')
+        call_command('migrate', interactive=False)
+
     execute_from_command_line(sys.argv)
 
 
